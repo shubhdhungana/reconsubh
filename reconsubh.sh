389f79712e71bd616f1a427e7a74e4a3b918d333
Shubham Dhungana <49474810+subhdhungana@users.noreply.github.com>
2021-11-24 14:15:19 +0545

    Add files via upload

--- reconsubh.sh
+++ reconsubh.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 
-###Name of the website to attack
-echo "Enter the target's name: "
-read target
+echo "Enter the name of website: "
+read domain
+echo " "
 
 
-##################this enumerates the subdomains and filter the live subdomains 
-assetfinder --subs-only $target | httpx | tee subdomain1.txt  
-subfinder -d $target | httpx | tee subdomain2.txt
-cat subdomain1.txt subdomain2.txt>allsubdomains.txt
-##########################Deleting unwanted subdomains file
-rm -rf subdomain1.txt subdomain2.txt 
+##subdomain enumeration
+assetfinder --subs-only $domain | tee  subdomainsAsset.txt
+subfinder -d $domain | tee subdomainsSubfinder.txt
+cat subdomainsAsset.txt subdomainsSubfinder.txt>subdomainsAll.txt
+rm -rf subdomainsAsset.txt subdomainsSubfinder.txt
 
-#######         fetching urls
-cat allsubdomains.txt | gau | tee urls1.txt
-cat allsubdomains.txt | gauplus | tee urls2.txt
-cat urls1.txt urls2.txt>urlsAll.txt
-###Deleting urls unwanted file
-rm -rf urls1.txt urls2.txt
+##live scanning of domains
+cat subdomainsAll.txt | httpx | tee  subdomainsLive.txt
 
-#####     filtering urls using gf tool
-cat urlsAll.txt | gf xss | tee xssgf.txt
-cat urlsAll.txt | gf ssrf | tee ssrfgf.txt
-cat urlsAll.txt | gf servers | tee serversgf.txt
-cat urlsAll.txt | gf aws-keys | tee awsgf.txt
 
-######organizing folder and moving files to proper folders
-mkdir urls
-mv awsgf.txt serersgf.txt ssrfgf.txt urlsAll.txt xssgf.txt urls/
 
-mv urlsAll.txt awsgf.txt serversgf.txt ssrfgf.txt xssgf.txt urls/ 
+## fetching urls
+cat subdomainsLive.txt | gau | tee urlsGau.txt
+cat subdomainsLive.txt | gauplus | tee urlsGauplus.txt
+cat urlsGau.txt urlsGauplus.txt>urlsAll.txt
+rm -rf urlsGau.txt urlsGauplus.txt
+
+
+##filter urls
+cat urlsAll.txt | gf xss | tee filterxss.txt
+cat urlsAll.txt | gf ssrf | tee filterssrf.txt
+cat urlsAll.txt | gf aws-keys | tee filteraws.txt
+
+##organizing
+mkdir -p $domain/subdomains $domain/urls $domain/livedomains
+mv subdomainsAll.txt $domain/subdomains/
+mv subdomainsLive.txt $domain/livedomains
+mv urlsAll.txt $domain/urls
+
+echo ""
+echo "  "
+echo " ####################### "
+echo " "
+echo "Task Completed"
